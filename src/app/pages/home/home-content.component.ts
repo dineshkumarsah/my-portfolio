d2c0efe4554c1abd39ad0fea2ee1e8a346f64044
dineshkumarsah <dineshdqg@gmail.com>
2025-07-26 12:22:48 +0530

    experience added

--- src/app/pages/home/home-content.component.ts
+++ src/app/pages/home/home-content.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
     selector:"app-home-cotent",
@@ -8,12 +9,12 @@ import { Component } from "@angular/core";
   <div>
     <p class="text-3xl font-bold">Hi, I am Dinesh Sah</p>
     <p class="mt-2 text-lg">
-      <span>Software Engineer III</span> | <span>Full Stack Developer</span>
+     <span>5.7 years of experience</span> | <span>Software Engineer III</span> | <span>Full Stack Developer</span>
     </p>
     <p class="mt-2 text-gray-400">
       Java, Spring Boot, MYSQL, MongoDB, J Unit | JavaScript, Angular 18, Jasmine & Karma | Docker , Kubernate8
     </p>
-    
+    <button (click)="experience()" class="border border-sky-500 text-sky-500 px-4 py-2 rounded hover:bg-sky-500 hover:text-white curssor-pointer">Experience</button>
   </div>
 </div>
 
@@ -21,4 +22,11 @@ import { Component } from "@angular/core";
     `,
     imports:[]
 })
-export class HomeContent{}
+export class HomeContent{
+    constructor(private routr :Router ){
+
+    }
+    experience(){
+     this.routr.navigate(['/experience'])
+    }
+}
